80f74da905a779ef77682c1022622264fe33b9eb
Mokhtar <149696460+privatemokhtar@users.noreply.github.com>
2023-12-06 21:51:40 +0330

    Update commands.go

--- commands.go
+++ commands.go
@@ -9,7 +9,7 @@ type ExampleCommand struct {
 	// Each parameter requires the field to be exported in order for the command to work properly.
 	// The `cmd:""` struct tag may be specified to change the name of the parameter and its suffix.
 	// Supported type: int*, uint*, float*, string, bool, mgl64.Vec3, []cmd.Target, cmd.Enum ...
-	Number int `cmd:"example"`
+	Number int `cmd:"number"`
 	// Wrapping the parameter type in `cmd.Optional` makes the parameter optional, meaning a cmd.Source
 	// does not need to supply it.
 	OptionalMessage cmd.Optional[string]
